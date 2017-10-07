c07d8f245aeda73f90c50a65a6b0fb887a407564
JuuliaJuutilainen <juulia.juutilainen@gmail.com>
2017-10-07 13:58:57 +0300

    korjataanhostit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7-p81u#@j23kpkead1ph+br-!jh$*i)xno9@yeq21@=wmh_01r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'juuliajuutilainen.pythonanywhere.com']
 
 
 # Application definition
